f08014e96815ad85e4a63ce826b85acde57a71ad
Shahadat <raselsha@gmail.com>
2024-08-20 12:24:33 +0600

    feat: playment plan pro ads

--- inc/admin/class-pro-ads.php
+++ inc/admin/class-pro-ads.php
@@ -16,15 +16,37 @@ class MEPP_Pro_Ads{
 
     public function __construct()
     {
-        add_action('admin_init', array($this, 'show_pro_advertise'));
+        add_action('init', array($this, 'show_pro_advertise'));
+         
     }
 
     public function show_pro_advertise(){
         if( ! defined('MEPP_PRO_VERSION_ACTIVE')){
             add_filter( 'mepp_settings_tabs', [$this,'show_advertise']) ; 
+            add_action( 'admin_menu', array($this, 'add_menu_payment_plan'));
         }
     }
+    public function add_menu_payment_plan(){
+        add_submenu_page(
+            'admin-mepp-deposits',       // Parent slug (the slug of the top-level menu)
+            'Payment plans',      // Page title
+            'Payment plans',         // Sub-menu title
+            'manage_options',       // Capability required to view this submenu
+            'payment-plans',      // Submenu slug (unique ID)
+            [$this,'payment_plans_ads'] // Callback function to display the submenu page content
+        );
+    }
 
+    public function payment_plans_ads(){
+        ?>
+        <div id="payment-plan-ads"> 
+            <div class="pro-ads">
+                <a target="_blank" href="<?php echo esc_attr(MEPP_Pro_Ads::get_purchase_link()) ?>" class="button button-primary"><?php _e('Buy pro','advanced-partial-payment-or-deposit-for-woocommerce'); ?></a>
+                <img src="<?php echo MEPP_PLUGIN_URL; ?>/assets/images/payment-plans.png" alt="" >
+            </div>
+        </div>
+    <?php
+    }
     public function show_advertise($menu){
         $ads_menu = array(
             'checkout_mode_ads' =>  '<i class="fas fa-money-check-alt"></i> ' . __('Checkout Mode', 'advanced-partial-payment-or-deposit-for-woocommerce') . '<i>&nbsp; PRO</i>',
